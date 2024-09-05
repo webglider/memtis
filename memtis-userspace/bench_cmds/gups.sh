64b0328fd2088bedfb05c4c598f6052458d6f730
Midhul <midhul.v@gmail.com>
2024-09-05 21:34:10 +0000

    minor update

--- memtis-userspace/bench_cmds/gups.sh
+++ memtis-userspace/bench_cmds/gups.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-BIN=/home/midhul/colloid/gups
+BIN=/home/midhul/colloid/apps/gups
 gups_cores=${MEMTIS_GUPS_CORES}
 BENCH_DRAM="32000MB"
 GUPS_DURATION=${MEMTIS_GUPS_DURATION}
 
 workload=${NVM_RATIO}
 
-BENCH_RUN="${BIN}/${workload} ${gups_cores}"
+if [ -n "${MEMTIS_GUPS_MOVE}" ]; then
+	BENCH_RUN="${BIN}/${workload} ${gups_cores} move ${MEMTIS_GUPS_MOVE}"
+elif [ -n "${MEMTIS_GUPS_DELAY_BG}" ]; then
+	BENCH_RUN="DELAY_BG=${MEMTIS_GUPS_DELAY_BG} ${BIN}/${workload} ${gups_cores}"
+else
+	BENCH_RUN="${BIN}/${workload} ${gups_cores}"
+fi
 
 
 export BENCH_RUN
