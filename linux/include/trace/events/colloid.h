2781ab0ca48b9424c771f39b66a77b51b6c04778
Midhul <midhul.v@gmail.com>
2024-09-09 02:02:21 +0000

    Update tracepoint definition

--- linux/include/trace/events/colloid.h
+++ linux/include/trace/events/colloid.h
@@ -8,8 +8,28 @@
 #include <linux/tracepoint.h>
 
 DECLARE_TRACE(colloid_migrate,
-        TP_PROTO(unsigned long nr_to_scan, unsigned long migrate_limit, unsigned long delta_p, unsigned long overall_accesses, bool promotion, unsigned long nr_migrated),
-        TP_ARGS(nr_to_scan, migrate_limit, delta_p, overall_accesses, promotion, nr_migrated));
+        TP_PROTO(unsigned long nr_to_scan, unsigned long migrate_limit, unsigned long delta_p, unsigned long overall_accesses, int promotion, unsigned long nr_migrated),
+        TP_ARGS(nr_to_scan, migrate_limit, delta_p, overall_accesses, promotion, nr_migrated),
+        TP_STRUCT__entry(
+           __field(unsigned long, nr_to_scan)
+           __field(unsigned long, migrate_limit)
+           __field(unsigned long, delta_p)
+           __field(unsigned long, overall_accesses)
+           __field(int, promotion)
+           __field(unsigned long, nr_migrated)
+        ),
+        TP_fast_assign(
+           __entry->nr_to_scan = nr_to_scan;
+           __entry->migrate_limit = migrate_limit;
+           __entry->delta_p = delta_p;
+           __entry->overall_accesses = overall_accesses;
+           __entry->promotion = promotion;
+           __entry->nr_migrated = nr_migrated;
+        ),
+
+	TP_printk("[colloid migration] nr_to_scan=%lu,migrate_limit=%lu,delta_p=%lu,overall_accesses=%lu,promotion=%d,nr_migrated=%lu",
+	    __entry->nr_to_scan, __entry->migrate_limit, __entry->delta_p, __entry->overall_accesses, __entry->promotion, __entry->nr_migrated)
+);
 
 #endif /* _TRACE_COLLOID_H */
 
