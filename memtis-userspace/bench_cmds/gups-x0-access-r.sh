34b3f6e5eef5fb8f2780ab8ad1718954071c0575
Omar Eqbal <eqbalomar@gmail.com>
2024-11-27 20:24:01 +0000

    script for x0 access

--- memtis-userspace/bench_cmds/gups-x0-access-r.sh
+++ memtis-userspace/bench_cmds/gups-x0-access-r.sh
@@ -2,13 +2,12 @@
 
 BIN=/home/omar/nucleus/apps
 gups_cores=16
-x_y_ratio=5
 duration=300
 wss=64
 
 BENCH_DRAM="32GB"
 
-BENCH_RUN="${BIN}/gups-xy-access-r ${gups_cores} ${duration} ${wss} ${x_y_ratio}"
+BENCH_RUN="${BIN}/gups-x0-access-r ${gups_cores} ${duration} ${wss}"
 
 export BENCH_RUN
 export BENCH_DRAM
