32bd7ea5bca9af85655c8acfdff2cc7d1b93d916
Omar Eqbal <eqbalomar@gmail.com>
2024-12-13 22:18:27 +0000

    update duration and wss

--- memtis-userspace/bench_cmds/gups-x0-access-r.sh
+++ memtis-userspace/bench_cmds/gups-x0-access-r.sh
@@ -2,10 +2,10 @@
 
 BIN=/home/omar/nucleus/apps
 gups_cores=16
-duration=300
-wss=64
+duration=7200
+wss=192
 
-BENCH_DRAM="32GB"
+BENCH_DRAM="96GB"
 
 BENCH_RUN="${BIN}/gups-x0-access-r ${gups_cores} ${duration} ${wss}"
 
